35fbf8e784a96b484d8065a6157bcc223e279e53
keside <realkeside@gmail.com>
2022-06-30 17:06:58 -0700

    another one

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ long long  longLongType;
 
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-printf("Size of a long: int %zu byte(s)\n", sizeof(longType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongType));
-printf("Size of a loat: %zu byte(s)\n", sizeof(floatType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 }
