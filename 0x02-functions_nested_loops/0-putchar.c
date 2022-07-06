08fcc29f59c0719e1a18387b50e3bf193d86c14a
keside <realkeside@gmail.com>
2022-07-05 20:58:32 -0700

    another one

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 #include "main.h"
 /**
  * main - tCheck descriptiom
@@ -12,10 +11,10 @@ int main(void)
 int i;
 char text[8] = "_putchar";
 for (i = 0; i < 8; i++)
-{
-_putchar(text[i]);
 
-}
-_putchar("\n");
+	_putchar(text[i]);
+
+
+_putchar('\n');
 return (0);
 }
